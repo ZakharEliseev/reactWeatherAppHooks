fde68c3c87b089e629608d09a580d5f0e2b61904
Ваше Имя <you@example.com>
2025-12-17 09:07:10 +0500

    refactor

--- src/components/WeatherForecast/index.tsx
+++ src/components/WeatherForecast/index.tsx
@@ -2,69 +2,70 @@ import { Circles } from 'react-loader-spinner';
 
 import { GroupedForecast, WeatherEntry } from '../WeatherCalendar/useCalendar';
 
-import styles from './index.module.scss';
+import classes from './index.module.scss';
 
-interface WeatherProps {
+interface Props {
   forecast: GroupedForecast;
   activeDay: any;
   isFetching: boolean;
   error: { data: { cod: string; message: string } };
 }
 
-export const Weather = ({ forecast, activeDay, isFetching, error }: WeatherProps) => {
-  const currentDay = forecast[activeDay];
-  const spinner = (
-    <Circles
-      height="40"
-      width="40"
-      color="#1875f0"
-      ariaLabel="circles-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-    />
-  );
+const Loader = () => (
+  <ul className={classes.weather}>
+    <li className={classes.loadData}>
+      <Circles height="40" width="40" color="#1875f0" visible />
+    </li>
+  </ul>
+);
 
-  return (
-    <>
-      <ul className={styles.weather}>
-        {!currentDay ? (
-          <>
-            <li className={!error ? styles.loadData : `${styles.loadData} header`}>
-              {error ? `${error.data.cod} ${error.data.message}` : 'Данные не были загружены.'}
-            </li>
-            <li className={styles.loadData}>{isFetching ? spinner : null}</li>
-          </>
-        ) : (
-          currentDay?.map((data: WeatherEntry) => (
-            <li key={data.time} className={styles.weatherItem}>
-              <div className={styles.weatherTemp}>
-                <p>{data.time}</p>
-                <p className={styles.weatherTempDegree}>{data.temp} °C</p>
-              </div>
-              <div className={styles.weatherDescr}>
-                <p>{data.description}</p>
-                <img
-                  className={styles.weatherDescrIcon}
-                  src={`./src/img/icons/${data.icon}@2x.png`}
-                />
-              </div>
-              <div className={styles.weatherWind}>
-                <p>Скорость ветра</p>
-                <p>{data.wind} м/с</p>
-              </div>
-              <div className={styles.weatherPressure}>
-                <p>Атмосферное давление</p>
-                <p>{data.pressure} мм рт.</p>
-              </div>
-              <div className={styles.weatherHumidity}>
-                <p>Влажность</p>
-                <p>{data.humidity}</p>
-              </div>
-            </li>
-          ))
-        )}
-      </ul>
-    </>
-  );
+const Stub = ({ error }: Pick<Props, 'error'>) => (
+  <ul className={classes.weather}>
+    <li className={!error ? classes.loadData : `${classes.loadData} header`}>
+      {error ? `${error.data.cod} ${error.data.message}` : 'Данные не были загружены.'}
+    </li>
+  </ul>
+);
+
+const List = ({ currentDay }: { currentDay: WeatherEntry[] }) => (
+  <ul className={classes.weather}>
+    {currentDay?.map(({ time, temp, description, icon, wind, pressure, humidity }: WeatherEntry) => (
+      <li key={time} className={classes.weatherItem}>
+        <div className={classes.weatherTemp}>
+          <p>{time}</p>
+          <p className={classes.weatherTempDegree}>{temp} °C</p>
+        </div>
+        <div className={classes.weatherDescr}>
+          <p>{description}</p>
+          <img className={classes.weatherDescrIcon} src={`./src/img/icons/${icon}@2x.png`} />
+        </div>
+        <div className={classes.weatherWind}>
+          <p>Скорость ветра</p>
+          <p>{wind} м/с</p>
+        </div>
+        <div className={classes.weatherPressure}>
+          <p>Атмосферное давление</p>
+          <p>{pressure} мм рт.</p>
+        </div>
+        <div className={classes.weatherHumidity}>
+          <p>Влажность</p>
+          <p>{humidity}</p>
+        </div>
+      </li>
+    ))}
+  </ul>
+);
+
+export const Weather = ({ forecast, activeDay, isFetching, error }: Props) => {
+  const currentDay = forecast[activeDay];
+
+  if (isFetching) {
+    return <Loader />;
+  }
+
+  if (!currentDay && error) {
+    return <Stub error={error} />;
+  }
+
+  return <List currentDay={currentDay} />;
 };
